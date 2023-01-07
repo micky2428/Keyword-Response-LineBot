9e245f7c83a93591bb9498b0876cd5e1dda04f71
micky2428 <99666209+micky2428@users.noreply.github.com>
2023-01-08 01:50:14 +0800

    Update line.py

--- line.py
+++ line.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('b+YZPLfqivSa3tY+uUr0nPP6tI4i3U6wChIrcjhIs6UlsdSF56k1S8E7EdDpvjRj8edR2U7bF9yDOjzcLqK7nG2ANizVg1XWHl+QxI8OLajHKteoE4Jprd3ZWRfDLgyIOll2KVNj6a0BbdckA0xtEwdB04t89/1O/w1cDnyilFU=', None))
-handler = WebhookHandler(os.getenv('fb9bfa5297b2c04a610fde0caf3f74a5', None)
+handler = WebhookHandler(os.getenv('fb9bfa5297b2c04a610fde0caf3f74a5', None))
 
                                          
 @app.route("/callback", methods=['POST'])
