9565505c3a7f0bb0ddcc3a65d84e08bb2ea59b71
micky2428 <99666209+micky2428@users.noreply.github.com>
2023-01-08 00:04:42 +0800

    Update LineBot_test.py

--- LineBot_test.py
+++ LineBot_test.py
@@ -1,4 +1,5 @@
 import os
+from pyChatGPT import ChatGPT
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
