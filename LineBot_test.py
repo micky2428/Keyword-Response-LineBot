ee463b3f93e6878b7e523318aa97aa27e9ad60d4
micky2428 <99666209+micky2428@users.noreply.github.com>
2023-01-11 22:14:28 +0800

    Update LineBot_test.py

--- LineBot_test.py
+++ LineBot_test.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
@@ -68,7 +67,6 @@ def handle_message(event):
 # if __name__ == "__main__":
 #     app.run()
       
-import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run() 
