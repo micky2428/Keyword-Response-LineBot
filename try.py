e1ebd346efb37a165ac81671d2b84885ce1bb5fc
micky2428 <99666209+micky2428@users.noreply.github.com>
2023-01-11 23:33:06 +0800

    Update try.py

--- try.py
+++ try.py
@@ -37,16 +37,35 @@ def callback():
 
     return 'OK'
 
-
+    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    mtext = event.message.text
+    if mtext =='＠使用說明':
     message = TextSendMessage(text='''歡迎使用小廢柴2.0🙌 
-         在這裡您將可以拯救你的眼睛～
-         請輸入查核表的編號🤖
-         您將收到對應的圖片''')
+    在這裡您將可以拯救你的眼睛～
+    請輸入查核表的編號🤖
+    您將收到對應的圖片''')
     line_bot_api.reply_message(
         event.reply_token,
-        message)
+        message)   
+    
+    elif re.match('1', message):
+     image_message = ImageSendMessage(
+        original_content_url='https://drive.google.com/drive/my-drive?hl=zh-tw',
+        preview_image_url='https://drive.google.com/drive/my-drive?hl=zh-tw'
+     )
+    line_bot_api.reply_message(event.reply_token, image_message)
+    
+    elif re.match('2', message):
+     image_message = ImageSendMessage(
+        original_content_url='https://i.imgur.com/9d0O0Jh.jpg',
+        preview_image_url='https://i.imgur.com/9d0O0Jh.jpg'
+     )
+    line_bot_api.reply_message(event.reply_token, image_message)
+
+            
+            
 
 if __name__ == "__main__":
     app.run()
