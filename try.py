9181a5b62859be31b5e32c8b48137254f99d4db8
micky2428 <99666209+micky2428@users.noreply.github.com>
2023-01-11 22:07:35 +0800

    Update try.py

--- try.py
+++ try.py
@@ -40,7 +40,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text=1)
     line_bot_api.reply_message(
         event.reply_token,
         message)
